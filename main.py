0b6eb4b07bb9f85646d640bb112dd03cab6996bb
Camillo-commits <kamil.osika@spoko.pl>
2022-05-04 22:54:29 +0200

    Copy of binary main

---
+++ main.py
@@ -0,0 +1,94 @@
+from random import randint, random
+
+from deap import base
+from deap import creator
+from deap import tools
+
+
+def individual(icls):
+    genome = list()
+    for x in range(0, 40):
+        genome.append(randint(0, 1))
+    return icls(genome)
+
+
+def fitnessFunction(individual):
+    # tutaj rozkoduj binarnego osobnika! Napisz funkcje decodeInd
+    ind = decodeInd(individual)
+    result = (ind[0] + 2 * ind[1] - 7) ** 2 + (2 * ind[0] + ind[1] - 5) ** 2
+    return result
+
+
+def decodeInd(individual):
+    # TODO
+    return list()
+
+
+def main_binary(is_min, selector, crosser, size_population, probability_mutation, probability_crossover, number_iteration, number_elitism):
+    if is_min:
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    else:
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+    toolbox = base.Toolbox()
+    toolbox.register('individual', individual, creator.Individual)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("evaluate", fitnessFunction)
+    toolbox.register("select", selector, tournsize=3)
+    toolbox.register("mate", crosser)
+
+    pop = toolbox.population(n=size_population)
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    g = 0
+    while g < number_iteration:
+        g = g + 1
+        print("-- Generation %i --" % g)
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+
+        listElitism = []
+        for x in range(0, number_elitism):
+            listElitism.append(tools.selBest(pop, 1)[0])
+
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            # cross two individuals with probability CXPB
+            if random.random() < probability_crossover:
+                toolbox.mate(child1, child2)
+                # fitness values of the children
+                # must be recalculated later
+                del child1.fitness.values
+                del child2.fitness.values
+        for mutant in offspring:
+            # mutate an individual with probability MUTPB
+            if random.random() < probability_mutation:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        print(" Evaluated %i individuals" % len(invalid_ind))
+        pop[:] = offspring + listElitism
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x * x for x in fits)
+        std = abs(sum2 / length - mean ** 2) ** 0.5
+        print(" Min %s" % min(fits))
+        print(" Max %s" % max(fits))
+        print(" Avg %s" % mean)
+        print(" Std %s" % std)
+        best_ind = tools.selBest(pop, 1)[0]
+        print("Best individual is %s, %s" % (best_ind,
+                                             best_ind.fitness.values))
+    #
+    print("-- End of (successful) evolution --")
