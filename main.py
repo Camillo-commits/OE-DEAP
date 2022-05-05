09e5e4b6d96f1906cbe5913edc2317ec053139c4
DamianZamojski <damian.zam1f20@gmail.com>
2022-05-05 20:07:22 +0200

    binary and real

--- main.py
+++ main.py
@@ -1,104 +1,5 @@
-
-from deap import base
-from deap import creator
 from deap import tools
-from numpy import random
-from random import randint
-
-
-def individual(icls):
-    genome = list()
-    for x in range(0, 40):
-        genome.append(randint(0, 1))
-    return icls(genome)
-
-
-def fitnessFunction(individual):
-    ind = decodeInd(individual)
-    result = ((ind[0] + 2 * ind[1] - 7) ** 2 + (2 * ind[0] + ind[1] - 5) ** 2,)
-    return result
-
-
-def decodeInd(individual):
-    ind = list()
-    middle = len(individual) // 2
-    ind.append(int(''.join(str(i) for i in individual[:middle]), 2))
-    ind.append(int(''.join(str(i) for i in individual[middle:]), 2))
-    return ind
-
-
-def main_binary(is_min, selector, crosser, mutator, size_population, probability_mutation, probability_crossover,
-                number_iteration, number_elitism):
-    if is_min:
-        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-        creator.create("Individual", list, fitness=creator.FitnessMin)
-
-    else:
-        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("Individual", list, fitness=creator.FitnessMax)
-
-    toolbox = base.Toolbox()
-    toolbox.register('individual', individual, creator.Individual)
-    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    toolbox.register("evaluate", fitnessFunction)
-    toolbox.register("select", selector, tournsize=3)
-    toolbox.register("mate", crosser)
-    toolbox.register("mutate", mutator, indpb = probability_mutation)
-
-    pop = toolbox.population(n=size_population)
-    fitnesses = list(map(toolbox.evaluate, pop))
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
-
-    g = 0
-    while g < number_iteration:
-        g = g + 1
-        print("-- Generation %i --" % g)
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        offspring = list(map(toolbox.clone, offspring))
-
-        listElitism = []
-        for x in range(0, number_elitism):
-            listElitism.append(tools.selBest(pop, 1)[0])
-
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            # cross two individuals with probability CXPB
-            if random.random() < probability_crossover:
-                toolbox.mate(child1, child2)
-                # fitness values of the children
-                # must be recalculated later
-                del child1.fitness.values
-                del child2.fitness.values
-        for mutant in offspring:
-            # mutate an individual with probability MUTPB
-            if random.random() < probability_mutation:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
-        print(" Evaluated %i individuals" % len(invalid_ind))
-        pop[:] = offspring + listElitism
-        # Gather all the fitnesses in one list and print the stats
-        fits = [ind.fitness.values[0] for ind in pop]
-        length = len(pop)
-        mean = sum(fits) / length
-        sum2 = sum(x * x for x in fits)
-        std = abs(sum2 / length - mean ** 2) ** 0.5
-        print(" Min %s" % min(fits))
-        print(" Max %s" % max(fits))
-        print(" Avg %s" % mean)
-        print(" Std %s" % std)
-        best_ind = tools.selBest(pop, 1)[0]
-        print("Best individual is %s, %s" % (best_ind,
-                                             best_ind.fitness.values))
-    #
-    print("-- End of (successful) evolution --")
+from binary.binary_representation import solve_binary_representation
 
 
 is_min = True
@@ -110,5 +11,6 @@ probability_mutation = 0.3
 probability_crossover = 0.5
 number_iteration = 10
 number_elitism = 1
-main_binary(is_min,selector,crosser,mutator, size_of_population,probability_mutation,probability_crossover,number_iteration,number_elitism)
+solve_binary_representation(is_min, selector, crosser, mutator, size_of_population, probability_mutation,
+            probability_crossover, number_iteration, number_elitism)
 print("Done!")
