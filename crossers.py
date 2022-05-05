39cad02c1c18c14b0307be075109b2a85f767bb4
Camillo-commits <kamil.osika@spoko.pl>
2022-05-05 19:45:29 +0200

    Added BLX_a_b_crosser

--- crossers.py
+++ crossers.py
@@ -11,3 +11,17 @@ def blx_a_crosser(first, second):
     new_second = random.uniform(smaller - alfa * d, bigger + alfa * d)
 
     return new_first, new_second
+
+
+def blx_a_b_crosser(first, second):
+    alfa = random.random()
+    beta = random.random()
+    d = abs(first - second)
+    smaller = min(first, second)
+    bigger = max(first, second)
+
+    new_first = random.uniform(smaller - alfa * d, bigger + beta * d)
+    new_second = random.uniform(smaller - alfa * d, bigger + beta * d)
+
+    return new_first, new_second
+
